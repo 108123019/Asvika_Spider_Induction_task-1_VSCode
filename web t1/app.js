42e0a7c581447cee4c7e549866a2e221a164c63b
108123019 <143599266+108123019@users.noreply.github.com>
2024-07-13 15:00:56 +0530

    Update app.js

--- web t1/app.js
+++ web t1/app.js
@@ -5,16 +5,30 @@ document.querySelector(tag).removeAttribute('hidden');
 document.querySelector("div.container").setAttribute("hidden",true);
 }
 
-function loadDoc(className){
-  let fl=className+"Recipe.txt";
-  var data=new XMLHttpRequest();
-  data.onreadystatechange=function(){
-    if(this.readyState==4 && this.status==200){
-      document.getElementById("details").innerHTML=this.responseText;
-      document.getElementById("details").scrollIntoView();
-    }
-  }
-  data.open("GET",fl);
-  data.send();
+//function loadDoc(className){
+//  let fl=className+"Recipe.txt";
+//  var data=new XMLHttpRequest();
+//  data.onreadystatechange=function(){
+//    if(this.readyState==4 && this.status==200){
+//      document.getElementById("details").innerHTML=this.responseText;
+//      document.getElementById("details").scrollIntoView();
+//    }
+//  }
+//  data.open("GET",fl);
+//  data.send();
+//}
+
+function loadDoc(className) {
+  const recipeFile = className + "Recipe.txt";
+
+  fetch(recipeFile)
+    .then(response => response.text()) // Parse response as text
+    .then(data => {
+      document.getElementById("details").innerHTML = data;
+      document.getElementById("details").scrollIntoView(); 
+    })
+    .catch(error => {
+      console.error("Error loading recipe:", error);
+    });
 }
 
